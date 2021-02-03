8d55fdbe6edef7c108e90f9909f55f82943788fc
Jan Steinmüller <janst1000@gmail.com>
2021-02-03 12:43:40 +0100

    finished problem 1

--- Week 1/a1_p1/test_array_search.cpp
+++ Week 1/a1_p1/test_array_search.cpp
@@ -26,7 +26,10 @@ int array_search(T* arr, int n, T elem){
 
 int main(int argc, char** argv){
 	int arr_size = 5;
+
 	int int_array[arr_size];
+	double d_array[arr_size];
+	char c_array[arr_size];
 	Complex cmp_array[arr_size];
 
 
@@ -35,17 +38,30 @@ int main(int argc, char** argv){
 		int_array[i] = i;
 	}
 
+	/*filling double array*/
+	for(int i = 0; i < arr_size; i++){
+		d_array[i] = i + (i * 0.1);
+	}
+
+	/*filling char array*/
+	for(int i = 0; i < arr_size; i++){
+		c_array[i] = 'a' + i;
+	}
+
 	/*filling Complex array*/
 	for(int i = 0; i < arr_size; i++){
 		cmp_array[i].setReal((double)i);
 		cmp_array[i].setImaginary((double)(i - 1));
 	}
 
+
 	/*creating Complex element to look for*/
 	Complex t(4, 3);
 
 	/*executing array_search and printing the results*/
 	cout << array_search(int_array, arr_size, 7) << endl;
+	cout << array_search(d_array, arr_size, 1.1) << endl;
+	cout << array_search(c_array, arr_size, 'a') << endl;
 	cout << array_search(cmp_array, arr_size, t) << endl;
 
 	return 0;
