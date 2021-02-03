2ad68cd0e25b5a034e43c63b08472708c741d7ae
Jan Steinmüller <janst1000@gmail.com>
2021-02-03 11:58:06 +0100

    working array_search function with ints

--- Week 1/a1_p1/test_array_search.cpp
+++ Week 1/a1_p1/test_array_search.cpp
@@ -9,6 +9,16 @@
 
 using namespace std;
 
+template <class T>
+int array_search(T* arr, int n, T elem){
+	for(int i = 0; i < n; i++){
+		if( arr[i] == elem){
+			return i;
+		}
+	}
+	return -1;
+}
+
 int main(int argc, char** argv){
 	int int_array[5];
 
@@ -17,10 +27,13 @@ int main(int argc, char** argv){
 		int_array[i] = i;
 	}
 
+	/*printing array*/
 	for(int i = 0; i < 5; i++){
 		cout << int_array[i];
 	}
 	cout << endl;
+
+	cout << array_search(int_array, 5, 3) << endl;
 
 	return 0;
 }
