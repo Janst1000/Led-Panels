de2d5c656bac527ea855f26e2d72b8c8e92a7013
Jan Steinmüller <janst1000@gmail.com>
2021-02-07 23:54:19 +0100

    added comments

--- Week 1/vectors1.cpp
+++ Week 1/vectors1.cpp
@@ -9,21 +9,29 @@
 
 using namespace std;
 int main(int argc, char** argv){
+	/*creating a vector*/
 	vector<string> vec;
+	/*initializing a string*/
 	string temp = "";
+	/*infinite loop for input*/
 	while(true){
 		cin >> temp;
+		/*checking if input is END*/
 		if(temp == "END"){
+			/*breaking out of the loop if it is*/
 			break;
 		}
+		/*otherwise we push the word to the back*/
 		vec.push_back(temp);
 	}
 
+	/*printing the words with the vector index*/
 	for(int i = 0; i < vec.size(); i++){
 		cout << vec[i] << " ";
 	}
 	cout << endl;
 
+	/*printing the word with the iterator*/
 	vector<string>::iterator vec_it = vec.begin();
 	while(vec_it != vec.end()){
 		cout << *vec_it;
