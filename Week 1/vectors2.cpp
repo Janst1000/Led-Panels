b00ad1e2dc2d313b717a67d2e784a650c01a41a5
Jan Steinmüller <janst1000@gmail.com>
2021-02-08 20:39:28 +0100

    modified makefile

---
+++ Week 1/vectors2.cpp
@@ -0,0 +1,13 @@
+/*
+    CH-231-A
+    vectors2.cpp
+    Jan Steinmueller
+    j.steinmueller@jacobs-university.de
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main(int argc, char** argv){
+	return 0;
+}
