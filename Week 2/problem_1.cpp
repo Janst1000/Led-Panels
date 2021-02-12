a3b1650c7895b0525c34e47bccde08953580df6e
Jan Steinmüller <janst1000@gmail.com>
2021-02-12 15:22:37 +0100

    working swap

--- Week 2/problem_1.cpp
+++ Week 2/problem_1.cpp
@@ -16,6 +16,7 @@ int main(int argc, char** argv){
 
 	int input;
 
+	/*getting inputs for the lists*/
 	while(true){
 		cin >> input;
 		if(input <= 0){
@@ -25,7 +26,99 @@ int main(int argc, char** argv){
 		B.push_front(input);
 	}
 
+	/*printing list 1 on the standard output*/
+	list<int>::iterator it = A.begin();
+	while(it != A.end()){
+		cout << *it << " ";
+		it++;
+	}
 	
+	/*opening listB.txt*/
+	ofstream out("listB.txt");
+	if(!out.good()){
+		cout << "error opening file listB.txt" << endl;
+		return 2;
+	}
+	/*printing list B in the file*/
+	it = B.begin();
+	while(it != B.end()){
+		out << *it << " ";
+		it++;
+	}
 
+	/*printing empty line as wanted in problem description*/
+	cout << endl << endl;
+
+	/*putting first element to the back for A*/
+	it = A.begin();
+	int pos = 0;
+	int temp = 0;
+	list<int>::iterator it2 = A.begin();
+	it2++;
+	while (it != A.end())
+	{
+		if(it == A.begin()){
+			temp = *it;
+		}
+		if(pos+1 == A.size()){
+			*it = temp;
+		} else {
+			*it = *it2;
+		}		
+		it++;
+		it2++;
+		pos++;
+	}
+
+	/*putting first element to the back for B*/
+	it = B.begin();
+	pos = 0;
+	temp = 0;
+	it2 = B.begin();
+	it2++;
+	while (it != B.end())
+	{
+		if(it == B.begin()){
+			temp = *it;
+		}
+		if(pos+1 == B.size()){
+			*it = temp;
+		} else {
+			*it = *it2;
+		}		
+		it++;
+		it2++;
+		pos++;
+	}
+
+	/*printing list A on the standard output*/
+	it = A.begin();
+	pos = 0;
+	while(it != A.end()){
+		cout << *it;
+		if(pos + 1 != A.size()){
+			cout << ",";
+		} else {
+			cout << endl;
+		}
+		it++;
+		pos++;
+	}
+	
+	/*printing list B on the standard output*/
+	it = B.begin();
+	pos = 0;
+	while(it != B.end()){
+		cout << *it;
+		if(pos + 1 != B.size()){
+			cout << ",";
+		} else {
+			cout << endl;
+		}
+		it++;
+		pos++;
+	}
+
+	out.close();
 	return 0;
 }
