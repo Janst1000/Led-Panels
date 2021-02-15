bd1ded9ffd561714ecbe064f539191fa76f7e205
Jan Steinmüller <janst1000@gmail.com>
2021-02-15 21:08:57 +0100

    finished problem 5

--- Week 2/problem_5.cpp
+++ Week 2/problem_5.cpp
@@ -7,9 +7,49 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <random>
+#include <ctime>
 
 using namespace std;
 
 int main(int argc, char** argv){
+	/*setting random seed*/
+	srand(time(NULL));
+
+	/*creating vector and helper variables*/
+	vector<int> A(6);
+	int num = 0;
+	bool included = false;
+
+	/*repeating until we have 6 different numbers*/
+	while(num < 6){
+		/*getting random value*/
+		int temp = rand() % 49 + 1;
+		/*checking if we have the value already*/
+		for(int i = 0; i < A.size(); i++){
+			if(temp == A[i]){
+				included = true;
+				break;
+			} else {
+				included = false;
+				
+			}
+		}
+		/*writing the value to the vector*/
+		if(included == false){
+			A[num] = temp;
+			num++;
+		}
+	}
+
+	/*sorting the vector*/
+	sort(A.begin(), A.end());
+
+	/*printing the vector*/
+	for(int i = 0; i < 6; i++){
+		cout << A[i] << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
