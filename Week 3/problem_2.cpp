c0f8928e716afb0e4d2c486e1cd2aa4360e14b4b
Jan Steinmüller <janst1000@gmail.com>
2021-02-21 23:36:02 +0100

    added comments

--- Week 3/problem_2.cpp
+++ Week 3/problem_2.cpp
@@ -16,28 +16,37 @@ using namespace std;
 */
 
 int main(int argc, char** argv){
+	/*intializing array*/
 	const int size = 10;
 	int arr[size];
 
+	/*setting seed to make reproducable results*/
 	srand(SEED);
 
+	/*filling the array with random numbers*/
 	for(int i = 0; i < size; i++){
 		arr[i] = rand() % 100;
 	}
 
+	/*printing array before sorting*/
 	for(int i = 0; i < size; i++){
 		cout << arr[i] << " ";
 	}
 
 	cout << endl;
 
+	/*start of selection sort*/
 	for(int i = 0; i < size -1; i++){
+		/*setting current element to minimum*/
 		int min = i;
+		/*going through the back of the array*/
 		for( int j = i +1; j < size; j++){
+			/*looking if there is a a smaller number*/
 			if(arr[j] < arr[min]){
 				min = j;
 			}
 		}
+		/*swapping if the minum number is not the current element*/
 		if (min != i ){
 			int temp = arr[min];
 			arr[min] = arr[i];
@@ -45,6 +54,7 @@ int main(int argc, char** argv){
 		}
 	}
 
+	/*printing array after sorting*/
 	cout << "finished sorting" << endl;
 
 	for(int i = 0; i < size; i++){
