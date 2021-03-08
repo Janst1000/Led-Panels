e13696ce3d3da4cf7576be969ac6c86355a8ec89
Jan Steinmüller <janst1000@gmail.com>
2021-03-08 09:34:22 +0100

    file creation for week 5

---
+++ Week 5/fibonacci.cpp
@@ -0,0 +1,16 @@
+/*
+    CH-231-A
+    fibonacci.cpp
+    Jan Steinmueller
+    j.steinmueller@jacobs-university.de
+*/
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+int main(int argc, char** argv){
+	
+
+	return 0;
+}
