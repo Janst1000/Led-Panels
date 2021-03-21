02dd92a451fedc0254d64c5481607c3f5da5e89b
Jan Steinmüller <janst1000@gmail.com>
2021-03-21 23:25:01 +0100

    intial file creation

---
+++ Week 7/problem1.cpp
@@ -0,0 +1,16 @@
+/*
+    CH-231-A
+    problem1.cpp
+    Jan Steinmueller
+    j.steinmueller@jacobs-university.de
+*/
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+int main(int arc, char** argv){
+	return 0;
+}
