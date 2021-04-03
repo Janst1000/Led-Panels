5d5c0c75c4c9cbefef43b02040fb7297db535ae4
Jan Steinmüller <janst1000@gmail.com>
2021-04-03 20:45:40 +0200

    not working bucket

--- Week 8/bucket_sort.cpp
+++ Week 8/bucket_sort.cpp
@@ -11,7 +11,59 @@
 using namespace std;
 using namespace chrono;
 
+void printArray(double A[], int n){
+	for(int i = 0; i < n; i++){
+		cout << A[i] << " ";
+	}
+	cout << endl;
+}
+
+void insertionSort(vector<double> bucket){
+	int length = bucket.size();
+	double key;
+	int j, k;
+	for(j = 1; j < length; j++){
+		key = bucket[j];
+		k = j - 1;
+		while (k >= 0 && bucket[k] > key)
+		{
+			bucket[k + 1] = bucket[k];
+			k--;
+		}
+		bucket[k + 1] = key;
+		
+	}
+}
+
+void bucketSort(double A[], int n){
+	vector<double> B[n];
+
+	/*fill buckets*/
+	for(int i = 0; i < n; i++){
+		int index = n * A[i];
+		B[index].push_back(A[i]);
+	}
+
+	/*insertionsort of buckets*/
+	for(int i = 0; i < n; i++){
+		insertionSort(B[i]);
+	}
+
+	int index;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < B[i].size(); j++){
+			A[index++] = B[i][j];
+		}
+	}
+}
+
 int main(int argc, char** argv){
+	int n = 7;
+	double A[n] = {0.9, 0.1, 0.6, 0.7, 0.6, 0.3, 0.1};
+
+	printArray(A, n);
+	bucketSort(A, n);
+	printArray(A, n);
 
 
 	return 0;
