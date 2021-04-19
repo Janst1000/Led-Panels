e7e707ad922c927ef992be208db7af72cc386e13
Jan Steinmüller <janst1000@gmail.com>
2021-04-19 21:27:37 +0200

    added right rotate

--- Week 10/RedBlackTree.cpp
+++ Week 10/RedBlackTree.cpp
@@ -6,12 +6,12 @@ RedBlackTree::RedBlackTree(){
 }
 
 void RedBlackTree::rotateLeft(Node *&x){
-	Node *y = x->right;
-	x->right = y->left;
+	Node *y = x->right;		//set y
+	x->right = y->left;		//turn y's subtree into x's right subtree
 	if(y->left != NULL){
 		y->left->parent = x;
 	}
-	y->parent = x->parent;
+	y->parent = x->parent;	//link x's parent to y
 	if(x->parent == NULL){
 		root = y;
 	}
@@ -21,6 +21,26 @@ void RedBlackTree::rotateLeft(Node *&x){
 	else {
 		y->parent->right = y;
 	}
-	y->left = x;
+	y->left = x;			//put x on y's left
+	x->parent;
+}
+
+void RedBlackTree::rotateRight(Node *&x){
+	Node *y = x->left;		//set y
+	x->left = y->right;		//turn y's subtree intox's left subtree
+	if(y->right != NULL){
+		y->right->parent = x;
+	}
+	y->parent = x->parent;	//link x's parent to y
+	if(x->parent == NULL){
+		root = y;
+	}
+	else if(x == x->parent->right){
+		x->parent->parent = y;
+	}
+	else {
+		y->parent->left = y;
+	}
+	y->right = x;			//put x on y's left
 	x->parent;
 }
