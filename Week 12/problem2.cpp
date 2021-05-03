7ce6db8d95c03a4cddc1e91d89df308c40ac3482
Jan Steinmüller <janst1000@gmail.com>
2021-05-03 22:07:53 +0200

    working triangle and clean up

--- Week 12/problem2.cpp
+++ Week 12/problem2.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
 #include <sstream>
-#include "Triangle.h"
+#include <vector>
+
 
 using namespace std;
 
+void printTri(vector<vector<int>> &tri){
+	int n = tri.size();
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j <= i; j++){
+			cout << tri[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
 int main(int argc, char** argv){
-	int temp;
-	cin >> temp;
-	Node *temp_node = createNode(temp, NULL);
-	Triangle tri(*temp_node);
-	cout << "success" << endl;
+	int lines;
+	cout << "How many lines does the triangle have?" << endl;
+	cin >> lines;
+	cin.clear();
+
+	vector<vector<int>> tri(lines);
+
+	for(int i = 0; i < lines; i++){
+		int temp = 0;
+		for(int j = 0; j <= i; j++){
+			cin >> temp;
+			tri[i].push_back(temp);
+			cout << "values: "<< i << " " << j << endl;
+		}
+		cin.clear();
+	}
+
+	printTri(tri);
+
 	return 0;
 }
